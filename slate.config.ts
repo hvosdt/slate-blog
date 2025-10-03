e50c04fb5d9212b708109a107a41dc63d210311b
hvosdt <hvosdt@gmail.com>
2025-10-03 11:02:26 +0300

    в

--- slate.config.ts
+++ slate.config.ts
@@ -4,12 +4,20 @@
 import { defineConfig } from './src/helpers/config-helper';
 
 export default defineConfig({
-  avatar: '/logo.png',
-  title: 'Читай, замышляй',
-  description: 'Стихи, сатира Давида Шуле.',
+  lang: 'en-US',
+  site: 'https://slate-blog-demo.vercel.app',
+  avatar: '/avatar.png',
+  title: 'Давид Шуле',
+  description: 'Читай, замышляй!',
   lastModified: true,
   readTime: true,
   footer: {
     copyright: '© 2025 Давид Шуле',
   },
+  socialLinks: [
+    {
+      icon: 'github',
+      link: 'https://github.com/SlateDesign/slate-blog'
+    },
+]
 });
